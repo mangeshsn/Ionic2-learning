cad47ebd01679e1ab09f01096e0cf603b85bc852
Mangesh Narkhede <mangesh.narkhede@capgemini.com>
2017-01-12 11:37:17 +0530

    Initial commit

---
+++ ionreddit/src/pages/reddits/reddits.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import {RedditService} from '../../app/services/reddit.service'
+import { NavController } from 'ionic-angular';
+import {DetailsPage} from '../details/details';
+
+@Component({
+  selector: 'reddits',
+  templateUrl: 'reddits.html'
+})
+export class RedditsPage {
+	items:any;
+  category: any;
+  limit: any; 
+  constructor(public navCtrl: NavController, private redditService:RedditService) {
+      this.getDefaults();
+  }
+  ngOnInit(){
+  	this.getPosts(this.category, this.limit);
+
+  }
+  getDefaults(){
+    this.category = "sports";
+    this.limit = 10;
+  }
+  getPosts(category, limit){
+  	this.redditService.getPosts(category,limit).subscribe(response => {
+  		this.items = response.data.children;
+  	});
+  }
+  viewItem(item){
+  	this.navCtrl.push(DetailsPage,{
+  		item:item
+  	});
+  }
+  changeCategory(){
+    this.getPosts(this.category, this.limit);
+  }
+
+}
